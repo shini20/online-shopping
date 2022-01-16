398da91c0320e35e0d1dd7f634dd92244529b4ec
shini20 <shinianitha99@gmail.com>
2022-01-16 21:25:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+});
+      
